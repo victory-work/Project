ad0dd9a0f5a52dc43377ab6a1da0a19f461c3813
LIAO <Leo900924@gmail.com>
2023-10-16 23:03:14 +0800

    regenerate cert_dataset

--- program/1_train_model.py
+++ program/1_train_model.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
 
     train_texts, eval_texts, train_labels, eval_labels = train_test_split(cert_texts, cert_labels, shuffle=True,
                                                                           test_size=0.4, random_state=42)
-    print(len(train_texts))
     tokenizer = BertTokenizer.from_pretrained(bert_model_name)
     train_dataset = TextClassificationDataset(
         train_texts, train_labels, tokenizer, max_length)
