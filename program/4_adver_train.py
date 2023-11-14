f6c3d7507d1561279a17c96b4faa595c4f87a41f
LIAO <Leo900924@gmail.com>
2023-11-14 11:55:12 +0800

    fix1

--- program/4_adver_train.py
+++ program/4_adver_train.py
@@ -1,40 +1,50 @@
 from func_def import *
-
 import pandas as pd
-from textattack.models.wrappers import BertModelWrapper
-from textattack.attack_recipes import BAE
-from textattack.datasets import TextCSVDataset
+import textattack
 
-def load_csv_dataset(csv_path):
-    # Load CSV dataset using pandas
-    df = pd.read_csv(csv_path)
-    
-    # Assuming your CSV has 'text' and 'label' columns
-    texts = df['text'].tolist()
-    labels = df['label'].tolist()
-    
-    return texts, labels
+bert_model_name = 'bert-base-uncased'
+num_classes = 2
+max_length = 128
+batch_size = 64
 
-def main():
-    # Step 1: Load BERT model
-    model_wrapper = BertModelWrapper(model_name="bert-base-uncased")
+model = BERTClassifier(bert_model_name, num_classes)
+tokenizer = BertTokenizer.from_pretrained(bert_model_name)
+model_wrapper = BERTClassifierWrapper(model, tokenizer)
 
-    # Step 2: Load the CSV dataset
-    csv_path = "double_quotes_train.csv"  # Replace with your CSV file path
-    texts, labels = load_csv_dataset(csv_path)
-    
-    # Step 3: Initialize the attack recipe
-    recipe = BAE.build(model_wrapper)
+# We only use DeepWordBugGao2018 to demonstration purposes.
+attack = textattack.attack_recipes.TextFoolerJin2019.build(model_wrapper)
+print(attack)
+csv_path = "double_quotes_train.csv"  # Replace with your CSV file path
+df = pd.read_csv(csv_path)
+print(df.shape)
 
-    # Step 4: Configure the attack
-    recipe = BAE.build(model_wrapper, transformations_per_example=4, max_candidates=10, max_attempts=3)
+# Split the dataset into training and evaluation sets
+train_df, eval_df = train_test_split(df, test_size=0.2, random_state=42)
 
-    # Step 5: Train the adversarial model
-    model_wrapper.train((texts, labels), recipe, num_epochs=3)
+# Create TextAttack datasets
+train_texts, train_labels = train_df['text'].tolist(), train_df['label'].tolist()
+eval_texts, eval_labels = eval_df['text'].tolist(), eval_df['label'].tolist()
 
-    # Step 6: Evaluate the adversarial model
-    accuracy = model_wrapper.eval((texts, labels))
-    print(f"Adversarial Model Accuracy: {accuracy}")
+train_dataset = textattack.datasets.Dataset(train_texts, train_labels)
+eval_dataset = textattack.datasets.Dataset(eval_texts, eval_labels)
 
-if __name__ == "__main__":
-    main()
+# Train for 3 epochs with 1 initial clean epochs, 1000 adversarial examples per epoch, learning rate of 5e-5, and effective batch size of 32 (8x4).
+training_args = textattack.TrainingArgs(
+    num_epochs=3,
+    num_clean_epochs=1,
+    num_train_adv_examples=100,
+    learning_rate=5e-5,
+    # per_device_train_batch_size=batch_size,
+    # gradient_accumulation_steps=1,
+    log_to_tb=True,
+)
+
+trainer = textattack.Trainer(
+    model_wrapper,
+    "classification",
+    attack,
+    train_dataset,
+    eval_dataset,
+    training_args
+)
+trainer.train()
