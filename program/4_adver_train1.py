8d271d22a2e97ab02f3981a3daca32c2d53e9709
LIAO <Leo900924@gmail.com>
2023-11-15 17:34:58 +0800

    fix4

--- program/4_adver_train1.py
+++ program/4_adver_train1.py
@@ -13,20 +13,18 @@ model_wrapper = BERTClassifierWrapper(model, tokenizer)
 
 # We only use DeepWordBugGao2018 to demonstration purposes.
 attack = textattack.attack_recipes.TextFoolerJin2019.build(model_wrapper)
-print(attack)
-csv_path = "double_quotes_train.csv"  # Replace with your CSV file path
+csv_path = "Project/double_quotes_train.csv"  # Replace with your CSV file path
 df = pd.read_csv(csv_path)
-print(df.shape)
 
 # Split the dataset into training and evaluation sets
 train_df, eval_df = train_test_split(df, test_size=0.2, random_state=42)
-train_dataset = textattack.datasets.HuggingFaceDataset(df, split="train")
-eval_dataset = textattack.datasets.HuggingFaceDataset(df, split="test")
 
-train_dataloader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True)
-eval_dataloader = DataLoader(eval_dataset, batch_size=batch_size)
+# # Create TextAttack datasets
+train_list = [(text, label) for text, label in zip(train_df["text"], train_df["label"])]
+eval_list = [(text, label) for text, label in zip(eval_df["text"], eval_df["label"])]
 
+train_dataset = textattack.datasets.Dataset(train_list,input_columns=("text"))
+eval_dataset = textattack.datasets.Dataset(eval_list,input_columns=("text"))
 
 # Train for 3 epochs with 1 initial clean epochs, 1000 adversarial examples per epoch, learning rate of 5e-5, and effective batch size of 32 (8x4).
 training_args = textattack.TrainingArgs(
@@ -35,7 +33,7 @@ training_args = textattack.TrainingArgs(
     num_train_adv_examples=100,
     learning_rate=5e-5,
     per_device_train_batch_size=batch_size,
-    gradient_accumulation_steps=1,
+    # gradient_accumulation_steps=1,
     log_to_tb=True,
 )
 
@@ -43,8 +41,8 @@ trainer = textattack.Trainer(
     model_wrapper,
     "classification",
     attack,
-    train_dataloader,
-    eval_dataloader,
+    train_dataset,
+    eval_dataset,
     training_args
 )
 trainer.train()
--- program/test.py
+++ program/test.py
@@ -6,17 +6,14 @@ csv_path = "double_quotes_train.csv"  # Replace with your CSV file path
 df = pd.read_csv(csv_path)
 print(df.shape)
 
-##### 問題...
-
-# Split the dataset into training and evaluation sets
 train_df, eval_df = train_test_split(df, test_size=0.2, random_state=42)
-# train_dataset = textattack.datasets.HuggingFaceDataset(df, split="train")
-# eval_dataset = textattack.datasets.HuggingFaceDataset(df, split="test")
 
-# # Create TextAttack datasets
-train_texts, train_labels = train_df['text'].tolist(), train_df['label'].tolist()
-eval_texts, eval_labels = eval_df['text'].tolist(), eval_df['label'].tolist()
 
-train_dataset = textattack.datasets.Dataset(train_texts, train_labels)
-eval_dataset = textattack.datasets.Dataset(eval_texts, eval_labels)
-print(train_dataset)
+train_list = [(text, label) for text, label in zip(train_df["text"], train_df["label"])]
+              
+data = [("A man inspects the uniform of a figure in some East Asian country.", "The man is sleeping", 1)]
+dataset = textattack.datasets.Dataset(data, input_columns=("premise", "hypothesis"))
+
+# Example for seq2seq
+data = [("J'aime le film.", "I love the movie.")]
+dataset = textattack.datasets.Dataset(data)
